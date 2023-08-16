355d7691b0fccab8806d8b08573310db62ea3489
sanglq3 <sanglq3@vng.com.vn>
2023-08-16 14:34:21 +0700

    Refactor code

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Avatar, Box, Text } from "zmp-ui";
+
+const UserAvatar = ({ userInfo }) => {
+  return (
+    <div className="flex items-center">
+      <Avatar src={userInfo?.avatar} size={48} />
+      <div>
+        <Text >{userInfo?.name}</Text>
+        <Text>{userInfo?.id}</Text>
+      </div>
+    </div>
+  );
+};
+
+export default UserAvatar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { Page } from 'zmp-ui';
+import React, { useEffect } from 'react';
+import { Button, Page, Avatar } from 'zmp-ui';
+import { getUserInfo } from "zmp-sdk/apis";
+import UserAvatar from "../components/UserAvatar";
+interface UserInfo {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
-const HomePage:React.FunctionComponent = () => {
+const HomePage: React.FunctionComponent = () => {
+  const [userInfo, setUserInfo] = React.useState<UserInfo | null>(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { userInfo } = await getUserInfo();
+        setUserInfo(userInfo);
+        console.log('data', userInfo);
+      } catch (error) {
+        // Handle API call failure
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <Page className="page">
-      Hello Zalo Mini App
+      <div className="page-content">
+        <UserAvatar userInfo={userInfo} />
+      </div>
     </Page>
   );
-}
+};
 
 export default HomePage;
