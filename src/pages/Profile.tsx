bc326bc16ff5a91c751a830042a5873d1e697650
sanglq3 <sanglq3@vng.com.vn>
2023-08-17 17:35:45 +0700

    Add some component

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,6 +4,24 @@ import styled from 'styled-components';
 import { Page, Box, Avatar, Text, Icon } from 'zmp-ui';
 
 export const ProfilePage: React.FunctionComponent = () => {
+
+  const Subscription: FC = () => {
+    return (
+      <Box className="m-4">
+        <Box
+          className="bg-green text-white rounded-xl p-4 space-y-2"
+          style={{
+            backgroundPosition: "right 8px center",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
+          <Text.Title className="font-bold">Đăng ký thành viên</Text.Title>
+          <Text size="xxSmall">Tích điểm đổi thưởng, mở rộng tiện ích</Text>
+        </Box>
+      </Box>
+    );
+  };
+
   const Personal: FC = () => {
     return (
       <Box className="m-4">
@@ -40,9 +58,47 @@ export const ProfilePage: React.FunctionComponent = () => {
     );
   };
 
+  const Other: FC = () => {
+    return (
+      <Box className="m-4">
+        <ListRenderer
+          title="Khác"
+          items={[
+            {
+              left: <Icon icon="zi-star" />,
+              right: (
+                <Box flex>
+                  <Text.Header className="flex-1 items-center font-normal">
+                    Đánh giá đơn hàng
+                  </Text.Header>
+                  <Icon icon="zi-chevron-right" />
+                </Box>
+              ),
+            },
+            {
+              left: <Icon icon="zi-call" />,
+              right: (
+                <Box flex>
+                  <Text.Header className="flex-1 items-center font-normal">
+                    Liên hệ và góp ý
+                  </Text.Header>
+                  <Icon icon="zi-chevron-right" />
+                </Box>
+              ),
+            },
+          ]}
+          renderLeft={(item) => item.left}
+          renderRight={(item) => item.right}
+        />
+      </Box>
+    );
+  };
+
   return (
     <Page>
+      <Subscription />
       <Personal />
+      <Other />
     </Page>
   );
 };
